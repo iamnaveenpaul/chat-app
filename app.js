40333c0e48c8fb01498edb9808935f338ce0f076
Naveen Paul <naveenpaul.markunda@gmail.com>
2019-08-01 20:06:16 +0530

    Adding post req

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 const ConversationClass = require('./controllers/conversation.js');
 const conversationObj = new ConversationClass();
 
@@ -15,8 +17,184 @@ mongoose.connect("mongodb://localhost/chat-app?poolSize=100",{ useNewUrlParser: 
     }
 });
 
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
     res.render('index.ejs');
+});
+
+app.get('/edit/username', function(req, res) {
+    res.render('userSettings.ejs');
+});
+
+app.post('/save/username', function(req, res) {
+    console.log(req.body);
+
+    res.send(req.body)
+});
+
+app.get('/get/messages/analytics', function(req, res) {
+    
+    conversationObj.getAllConversations(function(err,data){
+
+        var result = {}
+        var dateMin = req.query.dateMin;
+        var dateMax = req.query.dateMax;
+
+        if(!err && data && data.length>0){
+            data.forEach(function(el,index){
+                if(result[el.userName]){
+                    if(new Date(el.date)>=new Date(dateMin) && new Date(el.date)<=new Date(dateMax)){
+                        result[el.userName].count++;
+                        result[el.userName].messages.push(el)
+                    }
+            } else {
+
+                    if(new Date(el.date)>=new Date(dateMin) && new Date(el.date)<=new Date(dateMax)){
+                        result[el.userName] = {
+                            count:1,
+                            messages:[el]
+                        };
+                    }
+                };
+            })
+        }
+
+        res.send(result);
+    })
+});
+
+app.get('/get/messages/analytics/v2', function(req, res) {
+    
+    conversationObj.getConversationsAnalytics(req.query.range,function(err,data){
+
+        var monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+        // data.forEach(function(el){
+        //     el.month = monthNames[el._id.month-1]
+        //     el.monthYear = monthNames[el._id.month-1] +" "+ el._id.year
+        // })
+
+        res.send({
+            err:err,
+            data:data
+        });
+    })
+});
+
+app.get('/get/messages/analytics/v3', function(req, res) {
+
+    var dateMin = req.query.dateMin;
+
+    conversationObj.searchConversationsByDate(dateMin,function(err,data){
+        var result = {}
+
+        if(!err && data && data.length>0){
+            data.forEach(function(el,index){
+                if(result[el.userName]){
+                    result[el.userName].count++;
+                    result[el.userName].messages.push(el)
+            } else {
+
+                    result[el.userName] = {
+                        count:1,
+                        messages:[el]
+                    };
+                };
+            })
+        }
+
+        res.send(result);
+    })
+});
+
+app.get('/get/all/messages', function(req, res) {
+    
+    conversationObj.getAllConversations(function(err,data){
+        res.send(data);
+    })
+});
+
+app.get('/get/messages/for/user', function(req, res) {
+    
+    conversationObj.getConversationsForUser(req.query.userName,function(err,data){
+        res.send(data);
+    })
+});
+
+app.get('/get/messages/for/user/v2', function(req, res) {
+    
+    conversationObj.getAllConversations(function(err,data){
+
+        var filteredData = data.filter(function(el){
+            return el.userName == req.query.userName
+        })
+        .map(function(el){
+            return {
+                text:el.text,
+                date: el.date
+            };
+        })
+        .sort(function(el1,el2){
+            return new Date(el1.date)<new Date(el2.date);
+        })
+
+        res.send({
+            filteredData:filteredData
+        });
+    })
+});
+
+app.get('/get/messages/for/user/:id', function(req, res) {
+    conversationObj.getConversationsForUser(req.query.userName,function(err,data){
+        res.send(data);
+    })
+});
+
+app.get('/search/for/messages/for/user/:id', function(req, res) {
+
+    var searchStr = req.query.searchStr;
+    var userName = req.params.id;
+
+    conversationObj.searchConversationsForUser(userName,searchStr,function(err,data){
+        res.send(data);
+    })
+});
+
+app.get('/search/for/messages/for/user/date/:id', function(req, res) {
+
+    var searchStr = req.query.searchStr;
+    var dateMin = req.query.dateMin;
+    var userName = req.params.id;
+
+    conversationObj.searchConversationsForUserWithDate(userName,searchStr,dateMin,function(err,data){
+        res.send(data);
+    })
+});
+
+app.get('/search/for/messages/for/user/between/dates/:id', function(req, res) {
+
+    var searchStr = req.query.searchStr;
+    var dateMax = req.query.dateMax;
+    var dateMin = req.query.dateMin;
+    var userName = req.params.id;
+
+    conversationObj.searchConversationsForUserBetweenDates(userName,searchStr,dateMin,dateMax,function(err,data){
+        res.send(data);
+    })
+});
+
+app.post('/update/username', function(req, res) {
+
+    var existingUserName = req.body.existingUserName;
+    var newUserName = req.body.newUserName;
+
+    console.log(req.query);
+    console.log(req.body);
+
+    conversationObj.updateUserName(existingUserName,newUserName,function(err,data){
+        res.send(data);
+    })
 });
 
 io.sockets.on('connection', function(socket) {
--- controllers/conversation.js
+++ controllers/conversation.js
@@ -5,6 +5,128 @@ var ConversationCollection = require('../models/conversation').conversation;
 
 function Conversation() {}
 
+Conversation.prototype.getAllConversations = function(callback){
+    ConversationCollection
+    .find({})
+    .sort({date:1})
+    .exec(function(err,result){
+        callback(err,result)
+    })
+}
+
+Conversation.prototype.getConversationsAnalytics = function(range,callback){
+
+    var aggregationPipeline = [];
+
+    var groupRange = "$month";
+
+    if(range == "daily" || range == "day"){
+        groupRange = "$day";
+    } else if(range == "week" || range == "weekly"){
+        groupRange = "$week";
+    }
+    
+    var projection = {
+        $project: {
+            text: 1,
+            date: 1,
+            month: { $month: "$date" },
+            day: { $dayOfYear: "$date" },
+            week: { $week: "$date" },
+            year: { $year: "$date" }
+        }
+    }
+
+    var group = {
+        $group: {
+            _id: {
+                range: groupRange,
+                year: "$year"
+            },
+             totalMessages: { $sum: 1},
+             messages: {
+                 $push: {
+                     text:"$text",
+                     date: "$date"
+                 }
+             }
+        }
+    }
+
+    aggregationPipeline.push(projection)
+    aggregationPipeline.push(group)
+
+    ConversationCollection
+    .aggregate(aggregationPipeline)
+    .exec(function(err,result){
+        console.log(err);
+        callback(err,result)
+    })
+}
+
+Conversation.prototype.getConversationsForUser = function(userName,callback){
+    ConversationCollection.find({userName:userName}).exec(function(err,result){
+        callback(err,result)
+    })
+}
+
+Conversation.prototype.searchConversationsForUser = function(userName,searchStr,callback){
+
+    console.log(typeof userName);
+    console.log(typeof callback);
+
+    ConversationCollection.find({
+        userName:userName,
+        text:new RegExp(searchStr,"i")
+    })
+    .exec(function(err,result){
+        callback(err,result)
+    })
+}
+
+Conversation.prototype.searchConversationsForUserWithDate = function(userName,searchStr,date,callback){
+
+    ConversationCollection.find({
+        userName:userName,
+        text:new RegExp(searchStr,"i"),
+        date: {$gte: new Date(date)}
+    })
+    .exec(function(err,result){
+        callback(err,result)
+    })
+}
+
+Conversation.prototype.searchConversationsByDate = function(date,callback){
+
+    ConversationCollection.find({
+        date: {$gte: new Date(date)}
+    })
+    .exec(function(err,result){
+        callback(err,result)
+    })
+}
+
+Conversation.prototype.searchConversationsForUserBetweenDates = function(userName,searchStr,dateMin,dateMax,callback){
+
+    ConversationCollection.find({
+        userName:userName,
+        text:new RegExp(searchStr,"i"),
+        date: {$gte: new Date(dateMin),$lte:new Date(dateMax)}
+    })
+    .exec(function(err,result){
+        callback(err,result)
+    })
+}
+
+Conversation.prototype.updateUserName = function(existingUserName,newUserName,callback){
+
+    ConversationCollection
+    .update({userName:existingUserName},{$set:{userName:newUserName}},{multi:true})
+    .exec(function(err,result){
+        callback(err,result)
+    })
+}
+
 Conversation.prototype.storeConversation = function(data,callback){
     //Observe how we are referencing the Conversatoon Schema 
     // to create the conversation object before storing it.
